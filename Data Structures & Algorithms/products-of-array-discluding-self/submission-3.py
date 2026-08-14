5c6b32e951bba725e20052408114a07e14eb675e
hemantpacha <hemantpacha2020@gmail.com>
2026-08-14 15:11:47 -0400

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        #hashmap to store prod by index
+        products = {}
+        left = 1
+        #loop left to right calculating products
+        for i in range(0, len(nums)):
+            products[i] = left
+            left *= nums[i]
+
+        right = 1
+        for i in range(len(nums) - 1, -1, -1):
+            products[i] *= right
+            right *= nums[i]
+        
+        return list(products.values())
